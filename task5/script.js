6f272c520b48db0daec760716140e5ce51ded000
kbsu222w <qwerty249w@gmail.com>
2025-10-06 18:23:58 +0300

    Add files via upload

---
+++ task5/script.js
@@ -0,0 +1,15 @@
+
+
+function click1() {
+  let f2 = document.getElementsByName("select1");
+  let f1 = document.getElementsByName("field1");
+  let r = document.getElementById("result");
+  let num = f2[0].value
+  if (num == "t1") r.innerHTML = "Стоимость заказа: "+f1[0].value * 100;
+  else if (num == "t2") r.innerHTML = "Стоимость заказа: "+f1[0].value * 200;
+  else if (num == "t3") r.innerHTML = "Стоимость заказа: "+f1[0].value * 300;
+  
+  console.log(f2[0].value);
+  return false;
+}
+document.addEventListener("DOMContentLoaded", ready);
