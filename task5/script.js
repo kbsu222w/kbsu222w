9f0ac40422647f3925af5f73d0687bb352d5844e
kbsu222w <qwerty249w@gmail.com>
2025-10-12 21:21:38 +0300

    Update script.js

--- task5/script.js
+++ task5/script.js
@@ -18,12 +18,13 @@ let button1 = document.getElementById("button1");
         button1.addEventListener("click", click1);
     }
 
-})
+});
 
 
 
                       
                           
+
 
 
 
