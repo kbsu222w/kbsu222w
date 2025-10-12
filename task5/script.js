0f54aa4ce50cbc35a844d9a9ad6b2d0fcb56a28d
kbsu222w <qwerty249w@gmail.com>
2025-10-12 20:30:54 +0300

    Update script.js

--- task5/script.js
+++ task5/script.js
@@ -5,6 +5,7 @@ function click1() {
   let f1 = document.getElementsByName("field1");
   let r = document.getElementById("result");
   let num = f2[0].value
+  if (isNaN(f1[0].value)) r.innerHTML = "Ошибка, введите число";
   if (num == "t1") r.innerHTML = "Стоимость заказа: "+f1[0].value * 100;
   else if (num == "t2") r.innerHTML = "Стоимость заказа: "+f1[0].value * 200;
   else if (num == "t3") r.innerHTML = "Стоимость заказа: "+f1[0].value * 300;
@@ -12,4 +13,5 @@ function click1() {
   console.log(f2[0].value);
   return false;
 }
+
 document.addEventListener("DOMContentLoaded", ready);
