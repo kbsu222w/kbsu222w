4c5ec9468bdfdc261e620aab229b802df35e632a
kbsu222w <qwerty249w@gmail.com>
2025-10-12 20:48:43 +0300

    Update script.js

--- task5/script.js
+++ task5/script.js
@@ -1,6 +1,6 @@
 
 
-function click1() {
+document.addEventListener("DOMContentLoaded", click1() {
   let f2 = document.getElementsByName("select1");
   let f1 = document.getElementsByName("field1");
   let r = document.getElementById("result");
@@ -10,13 +10,11 @@ function click1() {
   else if (num == "t1") r.innerHTML = "Стоимость заказа: "+f1[0].value * 100;
   else if (num == "t2") r.innerHTML = "Стоимость заказа: "+f1[0].value * 200;
   else if (num == "t3") r.innerHTML = "Стоимость заказа: "+f1[0].value * 300;
-  
   console.log(f2[0].value);
-  return false;
-}
+  return false;})
 
-document.addEventListener("DOMContentLoaded", click1);
-
+                      
+                          
 
 
 
