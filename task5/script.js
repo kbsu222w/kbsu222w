759ddc1d610ebf5a8df7b3221a81eac613052cfb
kbsu222w <qwerty249w@gmail.com>
2025-10-12 21:20:34 +0300

    Update script.js

--- task5/script.js
+++ task5/script.js
@@ -4,18 +4,18 @@ function click1() {
   let f1 = document.getElementsByName("field1");
   let r = document.getElementById("result");
   let num = f2[0].value;
-  if (f1[0].value < 0) r.innerHTML = "Ошибка, введите положительное число";
-  else if (isNaN(f1[0].value)) r.innerHTML = "Ошибка, введите число";
-  else if (num == "t1") r.innerHTML = "Стоимость заказа: "+f1[0].value * 100;
-  else if (num == "t2") r.innerHTML = "Стоимость заказа: "+f1[0].value * 200;
-  else if (num == "t3") r.innerHTML = "Стоимость заказа: "+f1[0].value * 300;
+  if (f1[0].value < 0) {r.innerHTML = "Ошибка, введите положительное число";}
+  else if (isNaN(f1[0].value)) {r.innerHTML = "Ошибка, введите число";}
+  else if (num == "t1") {r.innerHTML = "Стоимость заказа: "+f1[0].value * 100;}
+  else if (num == "t2") {r.innerHTML = "Стоимость заказа: "+f1[0].value * 200;}
+  else if (num == "t3") {r.innerHTML = "Стоимость заказа: "+f1[0].value * 300;}
   console.log(f2[0].value);
   return false;
 }
-document.addEventListener('DOMContentLoaded', function() {
-let button1 = document.getElementById('button1');
+document.addEventListener("DOMContentLoaded", function() {
+let button1 = document.getElementById("button1");
     if (button1) {
-        button1.addEventListener('click', click1);
+        button1.addEventListener("click", click1);
     }
 
 })
@@ -24,6 +24,7 @@ let button1 = document.getElementById('button1');
 
                       
                           
+
 
 
 
